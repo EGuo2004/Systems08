e80cdd2381712c6fb8630d103560d6e7cb652f9d
unknown <>
2021-10-07 23:02:07 -0400

    Does more work

--- mystring.c
+++ mystring.c
@@ -12,11 +12,48 @@ int mystrlen( char *s ) {
 }
 
 char * mystrcpy( char *dest, char *source) {
-    int counter = 0;
-    int len = mystrlen(source);
-    while(counter < len) {
-        *(dest + counter) = *(source + counter);
-        counter++;
-    }
-    return *dest;
+	char *destcopy = dest; 
+    while(destcopy == dest || *(source - 1)) {
+		(*destcopy) = (*source);
+		source++;
+		destcopy++;
+	}
+	return *dest;
+}
+
+char * mystrncat( char *dest, char *source, int n) {
+	 int counter = 0;
+	 while(*dest){
+	 	dest++;
+	 }
+	 while(*source && (counter < n)) {
+	 	*(dest + counter) = *(source + counter);
+		counter++;
+	 }
+}
+
+int mystrcmp( char *s1, char *s2 ) {
+	while(*s1 || *s2) {
+		int v1 = *s1;
+		int v2 = *s2;
+		if(v1 > v2) {
+			return 1;
+		} else
+		if (v2 > v1) {
+			return -1;
+		}
+		s1++;
+		s2++;
+	}
+	return 0;
+}
+
+char * mystrchr( char *s, char c ) {
+	while(*s) {
+		if(*s == *c) {
+			return *s;
+		}
+		s++;
+	}
+	 return (c ? NULL : s);
 }
